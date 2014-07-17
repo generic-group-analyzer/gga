d4348dc710d2c480229d95ddabf145079c5e9cdf
Benedikt Schmidt <beschmi@gmail.com>
2014-07-17 09:48:43 +0200

    add nondet monad for synthesis

---
+++ src/Util/nondet.ml
@@ -0,0 +1,80 @@
+(* Nondeterminism Monad *)
+
+type 'a stream =
+    Nil
+  | Cons of 'a * (unit -> 'a stream)
+
+type 'a nondet = unit -> 'a stream
+
+let mempty = fun () -> Nil
+
+let ret a = fun () -> Cons (a, mempty)
+
+let guard pred =
+  if pred then ret () else mempty
+
+(* Combine results returned by a with results
+   returned by b. Results from a and b are
+   interleaved. *)
+let rec mplus a b = fun () ->
+  match a () with
+  | Cons (a1, a2) -> Cons (a1, mplus b a2)
+  | Nil           ->
+    begin match b () with
+    | Nil           -> Nil
+    | Cons (b1, b2) -> Cons (b1, mplus a b2)
+    end
+
+let rec bind m f = fun () ->
+  match m () with
+  | Nil         -> Nil
+  | Cons (a, b) -> mplus (f a) (bind b f) ()
+
+(* Execute and get first n results as list,
+   use n = -1 to get all values. *)
+let run n m =
+  let rec go n m acc =
+    if n = 0 then List.rev acc
+    else
+      match m () with
+      | Nil         -> List.rev acc
+      | Cons (a, b) -> go (pred n) b (a::acc)
+  in go n m []
+
+(* Apply function f to the first n values,
+   use n = -1 to apply f to all values. *)
+let iter n m f =
+  let rec go n m =
+    if n = 0 then ()
+    else
+      match m () with
+      | Nil         -> ()
+      | Cons (a, b) -> f a; go (pred n) b
+  in go n m
+
+
+let sequence ms =
+  let go m1 m2 =
+    bind m1 (fun x ->
+    bind m2 (fun xs ->
+    ret (x::xs)))
+  in
+  List.fold_right go ms (ret [])
+
+
+let mapM f ms = sequence (List.map f ms)
+let foreachM ms f = mapM f ms
+
+let rec msum ms =
+  match ms with
+  | m::ms -> mplus m (msum ms)
+  | []    -> mempty
+
+let rec mconcat ms =
+  match ms with
+  | m::ms -> mplus (ret m) (mconcat ms)
+  | []    -> mempty
+
+let (>>=) = bind
+
+let (>>) m1 m2 = m1 >>= fun _ -> m2
