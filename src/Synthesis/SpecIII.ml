05da5826dac9ce54987c563cce31969f8f0ebe99
Benedikt Schmidt <beschmi@gmail.com>
2015-09-08 14:13:56 +0200

    Make remaining specs for type III compile (untested)

--- src/Synthesis/SpecIII.ml
+++ src/Synthesis/SpecIII.ml
@@ -51,13 +51,13 @@ let spec1 () =
     { key_left    = [ v; w ];
       key_right   = [];
       msg_left_n  = [];
-      msg_right_n = ["M"];
-      sig_left    = [r];
-      sig_left_n  = ["R1"];
+      msg_right_n = [ "M" ];
+      sig_left    = [ r ];
+      sig_left_n  = [ "R1" ];
       sig_right   = [ r2_poly; s_poly ];
-      sig_right_n = ["R2"; "S"];
+      sig_right_n = [ "R2"; "S" ];
       setting     = TY3;
-      osample     = ["R"]
+      osample     = [ "R" ]
     }
   in
 
@@ -89,7 +89,7 @@ let spec1 () =
   }
 
 (* Type III: verification equation S*R = f(R,V,W,M), T=g(R,V,W) *)
-let spec2 = spec1 (*
+let spec2 () = 
   let cS_v   = gen () in
   let cS_w   = gen () in
   let cS_m   = gen () in
@@ -107,6 +107,12 @@ let spec2 = spec1 (*
   let cT1_plus_w = gen () in
   let cT1_plus_v_w = gen () in
 
+  let t1_poly =
+      (cT1_rand     * r)
+    + (cT1_plus_v   * (r + v))
+    + (cT1_plus_w   * (r + w))
+    + (cT1_plus_v_w * (r + v + w))
+  in 
   let s_poly =
       (cS_v * v)
     + (cS_w * w)
@@ -119,57 +125,54 @@ let spec2 = spec1 (*
     + (cS_rw * r *@ w)
     + (cS_r * r)
   in
-  let t1_poly =
-      (cT1_rand * r)
-    + (cT1_plus_v   * r +@ v)
-    + (cT1_plus_w   * r +@ w)
-    + (cT1_plus_v_w * r +@ v +@ w)
-  in
-  let sps_t =
-    { key_left    = [v; w];
+ let sps_t =
+    { key_left    = [ v; w ];
       key_right   = [];
       msg_left_n  = [];
-      msg_right_n = ["M"];
-      sig_left    = [ladd t1_poly];
-      sig_left_n  = ["T1"];
-      sig_right   = [ladd t1_poly; ladd s_poly *@ ri ];
-      sig_right_n = ["T2"; "S"];
+      msg_right_n = [ "M" ];
+      sig_left    = [ t1_poly ];
+      sig_left_n  = [ "T1" ];
+      sig_right   = [ t1_poly; s_poly *@ ri ];
+      sig_right_n = [ "T2"; "S" ];
       setting     = TY3;
-      osample     = ["R"]
+      osample     = [ "R" ]
     }
   in
 
   (* r always used since s = ... / r *)
   let nonzero_constrs =
-    [ [cS_vm;cS_wm;cS_rm;cS_m]                      (* m used *)
-    ; [cT1_rand;cT1_plus_v;cT1_plus_w;cT1_plus_v_w] (* one of the choices for T1 *)
-    (* ; [cS_v;cS_vm;cS_rv;cR_plus_v;cR_plus_v_w]   (* v used *)
-       ; [cS_w;cS_wm;cS_rw;cR_plus_w;cR_plus_v_w]   (* w used *) *)
+    [ cS_vm + cS_wm + cS_rm +cS_m                        (* m used *)
+    ; cT1_rand + cT1_plus_v + cT1_plus_w + cT1_plus_v_w (* one of the choices for T1 *)
+    (* ; [cS_v;cS_vm;cS_rv;cR_plus_v;cR_plus_v_w]       (* v used *)
+       ; [cS_w;cS_wm;cS_rw;cR_plus_w;cR_plus_v_w]       (* w used *) *)
     ]
   in
   (* not more than one of the choices for R *)
   let zero_constrs =
-    [ [cT1_rand;cT1_plus_v]
-    ; [cT1_rand;cT1_plus_w]
-    ; [cT1_rand;cT1_plus_v_w]
-    ; [cT1_plus_v;cT1_plus_w]
-    ; [cT1_plus_v;cT1_plus_v_w]
-    ; [cT1_plus_w;cT1_plus_v_w]
+    [ cT1_rand   * cT1_plus_v
+    ; cT1_rand   * cT1_plus_w
+    ; cT1_rand   * cT1_plus_v_w
+    ; cT1_plus_v * cT1_plus_w
+    ; cT1_plus_v * cT1_plus_v_w
+    ; cT1_plus_w * cT1_plus_v_w
     ]
   in
-  let symmetries = [ (cS_v,cS_w); (cS_vm,cS_wm) ] in
-  { sps_t = sps_t;
-    vars = !vars;
-    symmetries = symmetries;
+  let symmetries =
+    ([ t1_poly; s_poly ],
+     [ (* [ (cS_v,cS_w); (cS_vm,cS_wm) ] *)
+     ])
+  in
+  { sps_t           = sps_t;
+    choices         = [0; 1];
+    vars            = !vars;
+    symmetries      = symmetries;
     nonzero_constrs = nonzero_constrs;
-    zero_constrs = zero_constrs
+    zero_constrs    = zero_constrs;
+    equivsigs       = []
   }
-  *)
 
 (* Type III: verification equation S*R = f(R,V,W,M), T=g(R,V,W) *)
-let spec3 = spec1
-(*
-  let (vars,gen) = var_gen () in
+let spec3 () =
   let cS_v   = gen () in
   let cS_w   = gen () in
   let cS_m   = gen () in
@@ -182,28 +185,34 @@ let spec3 = spec1
   let cS_1   = gen () in
 
   let s_poly =
-    L.map mult_var
-      [ (cS_v, v); (cS_w, w); (cS_m, m); (cS_1, from_int 1)
-      ; (cS_vm, v *@ m); (cS_wm, w *@ m); (cS_rm, r *@ m)
-      ; (cS_rv, r *@ v); (cS_rw, r *@ w); (cS_r, r) ]
+       (cS_v  * v)
+     + (cS_w  * w)
+     + (cS_m  * m)
+     + (cS_1  * from_int 1)
+     + (cS_vm * v * m)
+     + (cS_wm * w * m)
+     + (cS_rm * r * m)
+     + (cS_rv * r * v)
+     + (cS_rw * r * w)
+     + (cS_r  * r)
   in
   let sps_t =
-    { key_left    = [v; w];
+    { key_left    = [ v; w ];
       key_right   = [];
       msg_left_n  = [];
-      msg_right_n = ["M"];
-      sig_left    = [r];
-      sig_left_n  = ["T1"];
-      sig_right   = [ri; ladd s_poly *@ ri ];
-      sig_right_n = ["T2"; "S"];
+      msg_right_n = [ "M" ];
+      sig_left    = [ r ];
+      sig_left_n  = [ "T1" ];
+      sig_right   = [ ri; s_poly * ri ];
+      sig_right_n = [ "T2"; "S" ];
       setting     = TY3;
-      osample     = ["R"]
+      osample     = [ "R" ]
     }
   in
 
   (* r always used since s = ... / r *)
   let nonzero_constrs =
-    [ [cS_vm;cS_wm;cS_rm;cS_m]                      (* m used *)
+    [ cS_vm + cS_wm +cS_rm  + cS_m                    (* m used *)
     (* ; [cS_v;cS_vm;cS_rv;cR_plus_v;cR_plus_v_w]   (* v used *)
        ; [cS_w;cS_wm;cS_rw;cR_plus_w;cR_plus_v_w]   (* w used *) *)
     ]
@@ -212,20 +221,23 @@ let spec3 = spec1
   let zero_constrs =
     [ ]
   in
-  let symmetry = [ (cS_v,cS_w); (cS_vm,cS_wm) ] in
-  { sps_t = sps_t;
-    vars = !vars;
-    symmetry = symmetry;
+  let symmetries =
+    ( []
+    , [ [ (cS_v,cS_w); (cS_vm,cS_wm) ]
+      ])
+  in
+  { sps_t           = sps_t;
+    choices         = [ 0; 1 ];
+    vars            = !vars;
+    symmetries      = symmetries;
     nonzero_constrs = nonzero_constrs;
-    zero_constrs = zero_constrs
+    zero_constrs    = zero_constrs;
+    equivsigs       = [];
   }
-*)
 
 
 (* Type III: verification equation S*R = f(R,V,W,M), T=g(R,V,W). mixed keys *)
-let spec4 = spec1
-(*
-  let (vars,gen) = var_gen () in
+let spec4 () =
   let cS_v   = gen () in
   let cS_w   = gen () in
   let cS_m   = gen () in
@@ -244,56 +256,65 @@ let spec4 = spec1
   let cT1_plus_v_w = gen () in
 
   let s_poly =
-    L.map mult_var
-      [ (cS_v, v); (cS_w, w); (cS_m, m); (cS_1, from_int 1)
-      ; (cS_vm, v *@ m); (cS_wm, w *@ m); (cS_rm, r *@ m)
-      ; (cS_rv, r *@ v); (cS_rw, r *@ w); (cS_r, r) ]
+      (cS_v  * v)
+    + (cS_w  * w)
+    + (cS_m  * m)
+    + (cS_1  * from_int 1)
+    + (cS_vm * v * m)
+    + (cS_wm * w * m)
+    + (cS_rm * r * m)
+    + (cS_rv * r * v)
+    + (cS_rw * r * w)
+    + (cS_r  * r)
   in
   let t1_poly =
-    L.map mult_var
-      [ (cT1_rand, r)
-      ; (cT1_plus_v, r +@ v)
-      ; (cT1_plus_w, r +@ w)
-      ; (cT1_plus_v_w, r +@ v +@ w)
-      ]
+      (cT1_rand     * r)
+    + (cT1_plus_v   * (r + v))
+    + (cT1_plus_w   * (r + w))
+    + (cT1_plus_v_w * (r + v + w))
   in
   let sps_t =
-    { key_left    = [v];
-      key_right   = [w];
+    { key_left    = [ v ];
+      key_right   = [ w ];
       msg_left_n  = [];
-      msg_right_n = ["M"];
-      sig_left    = [ladd t1_poly];
-      sig_left_n  = ["T1"];
-      sig_right   = [ladd t1_poly; ladd s_poly *@ ri ];
-      sig_right_n = ["T2"; "S"];
+      msg_right_n = [ "M" ];
+      sig_left    = [ t1_poly ];
+      sig_left_n  = [ "T1" ];
+      sig_right   = [ t1_poly; s_poly * ri ];
+      sig_right_n = [ "T2"; "S" ];
       setting     = TY3;
-      osample     = ["R"]
+      osample     = [ "R" ]
     }
   in
 
   (* r always used since s = ... / r *)
   let nonzero_constrs =
-    [ [cS_vm;cS_wm;cS_rm;cS_m]                      (* m used *)
-    ; [cT1_rand;cT1_plus_v;cT1_plus_w;cT1_plus_v_w] (* one of the choices for T1 *)
+    [ cS_vm + cS_wm + cS_rm + cS_m                       (* m used *)
+    ; cT1_rand + cT1_plus_v + cT1_plus_w + cT1_plus_v_w  (* one of the choices for T1 *)
     (* ; [cS_v;cS_vm;cS_rv;cR_plus_v;cR_plus_v_w]   (* v used *)
        ; [cS_w;cS_wm;cS_rw;cR_plus_w;cR_plus_v_w]   (* w used *) *)
     ]
   in
   (* not more than one of the choices for R *)
   let zero_constrs =
-    [ [cT1_rand;cT1_plus_v]
-    ; [cT1_rand;cT1_plus_w]
-    ; [cT1_rand;cT1_plus_v_w]
-    ; [cT1_plus_v;cT1_plus_w]
-    ; [cT1_plus_v;cT1_plus_v_w]
-    ; [cT1_plus_w;cT1_plus_v_w]
+    [ cT1_rand   * cT1_plus_v
+    ; cT1_rand   * cT1_plus_w
+    ; cT1_rand   * cT1_plus_v_w
+    ; cT1_plus_v * cT1_plus_w
+    ; cT1_plus_v * cT1_plus_v_w
+    ; cT1_plus_w * cT1_plus_v_w
     ]
   in
-  let symmetry = [ (cS_v,cS_w); (cS_vm,cS_wm) ] in
-  { sps_t = sps_t;
-    vars = !vars;
-    symmetry = symmetry;
+  let symmetries =
+    ( []
+    , [ [ (cS_v,cS_w); (cS_vm,cS_wm) ]
+      ])
+  in
+  { sps_t           = sps_t;
+    choices         = [0; 1];
+    vars            = !vars;
+    symmetries      = symmetries;
     nonzero_constrs = nonzero_constrs;
-    zero_constrs = zero_constrs
+    zero_constrs    = zero_constrs;
+    equivsigs       = [];
   }
-*)
